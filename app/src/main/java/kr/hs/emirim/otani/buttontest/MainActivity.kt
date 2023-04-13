8909f03a71ea92956503a07da644e5318b61c5f5
Jian Jang <pureunnabi@gmail.com>
2023-04-13 20:04:44 +0900

    기본

--- app/src/main/java/kr/hs/emirim/otani/buttontest/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/otani/buttontest/MainActivity.kt
@@ -7,8 +7,9 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var btn1 : Button
-        btn1 = findViewById<Button>(R.id.btn1)
+//        setContentView(R.layout.activity_main)
+//        var btn1 : Button
+//        btn1 = findViewById<Button>(R.id.btn1)
+        setContentView(R.layout.layout3)
     }
 }
